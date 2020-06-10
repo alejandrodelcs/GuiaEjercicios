633657091fe1e12259235d4932bbb8b99cc27efb
alejandrodelcs <alejodc15@gmail.com>
2020-06-10 14:44:34 -0300

    -

--- parcial2020/ejercicio1.py
+++ parcial2020/ejercicio1.py
@@ -2,17 +2,19 @@
 
 def cant_letras_numeros(clave):
     num_cadena = [x for x in clave if x.isdigit()]
-    let_cadena = [y for y in set(clave.lower()) if y.isalpha()]
+    let_cadena = [y for y in clave.lower() if y.isalpha()]
+
     if len(let_cadena) > len(num_cadena):
         valido = True
     else:
         valido = False 
 
 def caracteres_validacion(clave):
-    if clave[:1].isdigit() == True and clave[-3:len(clave)].isdigit() == False:
+    if clave[:1].isdigit() == True and clave[-3:].isdigit() == False:
         valido = True
     else:
         valido = False 
+    return valido
 
 def sumar_numeros(clave):
     suma = 0
@@ -24,13 +26,23 @@ def sumar_numeros(clave):
 
 
 def ingreso_clave(clave):
+    """
+    >>> ingreso_clave("695abcdef")
+    True
+    >>> ingreso_clave("XkM485Pedr0")
+    True
+    >>> ingreso_clave("pl485936")
+    False
+    >>> ingreso_clave("pedro452rar")
+    False
+    """
 
-    if len(clave) >=8 and len(clave)<=12 and sumar_numeros(clave)<35:
+    if (len(clave) >=8 and len(clave)<=12) and sumar_numeros(clave)<=35:
         valido = True
     else:
         valido = False 
 
     return valido
 
-clave = '123654'
-print(ingreso_clave(clave))
+import doctest
+doctest.testmod()
